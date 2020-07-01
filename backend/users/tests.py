845de8f00f46a399ebb77ac2094d889730bf5a21
Aleksei Semenov <7.on.off@bk.ru>
2020-07-01 17:11:38 +0300

    adds fix env

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -9,4 +9,4 @@ from backend.users.apps import UsersConfig
 class TestApp(TestCase):
     def test_apps(self: TestApp) -> None:
         assert UsersConfig.name == "users"
-        assert apps.get_app_config("users").name == "backend.users"
+        assert apps.get_app_config("users").name == "users"
